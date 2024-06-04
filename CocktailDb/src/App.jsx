7d21523f5f5d7628396503956df0b0b37ffa2365
Sikandar mohammad <rightmentor@gmail.com>
2024-06-04 16:55:50 +0530

    admin

--- CocktailDb/src/App.jsx
+++ CocktailDb/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Spinner from './components/Spinner'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
+    <Spinner></Spinner>
     </>
   )
 }
---
+++ CocktailDb/src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+const Spinner=()=>{
+
+  return<><br/><div className="spinner-border loading-spinner" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div></>
+};
+
+export default Spinner;
